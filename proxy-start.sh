0eea99862aeef5650a6f67cf1a9b59fc70846b44
Alexander Sitnik <alex.sitnik@gmail.com>
2017-11-02 19:09:15 +0300

    nginx: add HTTPS with autogenerated certs

--- proxy-start.sh
+++ proxy-start.sh
@@ -3,4 +3,15 @@
 set -e
 set -x
 
+if [ ! -f /etc/ssl/dhparam.pem ]
+then
+    # NOTE: we only need this if we want to support non-PFS ciphers
+    openssl dhparam -out /etc/ssl/dhparam.pem 2048
+fi
+
+if [ ! -f /etc/ssl/ssl_certificate -o ! -f /etc/ssl/ssl_certificate_key ]
+then
+    openssl req -x509 -newkey rsa:2048 -keyout /etc/ssl/ssl_certificate_key -out /etc/ssl/ssl_certificate -days 365 -nodes -subj '/CN=localhost' -sha256
+fi
+
 nginx
