e5c0ce76eef4a7c785ce0b2dfba14f49364aa694
Alexander Sitnik <alex.sitnik@gmail.com>
2017-11-15 12:22:50 +0300

    nginx: fixup startup script

--- proxy-start.sh
+++ proxy-start.sh
@@ -14,6 +14,6 @@ then
     openssl req -x509 -newkey rsa:2048 -keyout /etc/ssl/ssl_certificate_key -out /etc/ssl/ssl_certificate -days 365 -nodes -subj '/CN=localhost' -sha256
 fi
 
-echo resolver $(aws 'BEGIN{ORS=" "} $1=="nameserver" {print $2}' /etc/resolv.conf) ";" > /etc/nginx/resolvers.conf
+echo resolver $(awk 'BEGIN{ORS=" "} $1=="nameserver" {print $2}' /etc/resolv.conf) ";" > /etc/nginx/resolvers.conf
 
 nginx
