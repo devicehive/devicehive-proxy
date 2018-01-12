76439da2ab7f869f9a1943166be5962f400f8b8a
Alexander Sitnik <alex.sitnik@gmail.com>
2018-01-12 13:18:43 +0300

    nginx: add dynamic configuration for external ws-proxy

--- proxy-start.sh
+++ proxy-start.sh
@@ -16,8 +16,14 @@ fi
 
 echo resolver $(awk 'BEGIN{ORS=" "} $1=="nameserver" {print $2}' /etc/resolv.conf) ";" > /etc/nginx/resolvers.conf
 
+# Enable dh_plugin configuration only if dh_plugin resolvable
 getent hosts dh_plugin \
   && ln -s /etc/nginx/upstreams-available/plugin_upstream.conf /etc/nginx/upstream.d/plugin_upstream.conf \
   && ln -s /etc/nginx/locations-available/plugin_location.conf /etc/nginx/location.d/plugin_location.conf
+
+# Enable wsproxyext configuration only if wsproxyext resolvable
+getent hosts wsproxyext \
+  && ln -s /etc/nginx/upstreams-available/wsproxyext.conf /etc/nginx/upstream.d/wsproxyext.conf \
+  && ln -s /etc/nginx/locations-available/wsproxyext.conf /etc/nginx/location.d/wsproxyext.conf
 
 nginx
